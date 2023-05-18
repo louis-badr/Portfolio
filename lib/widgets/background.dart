fb66d3fca30b8ecbfbd93c4cc20d9a83038d9a60
Louis BADR <louis.badr@edu.devinci.fr>
2023-05-19 01:14:37 +0200

    Background borders done

--- lib/widgets/background.dart
+++ lib/widgets/background.dart
@@ -2,65 +2,44 @@ import 'package:flutter/material.dart';
 
 class GridPainter extends CustomPainter {
   final int margin = 20;
+  final int marksNb = 4;
   @override
   void paint(Canvas canvas, Size size) {
-    double rulesSpacing = (size.width - margin) / 4;
-    // Vertical line
-    Paint paint_0 = Paint()
+    final double vLineLength = size.height - margin;
+    final double hLineLength = size.width - margin;
+    final double markLength = margin / 2;
+    final double rulesSpacing = hLineLength / (marksNb + 1);
+    final int vMarksNb = (vLineLength % rulesSpacing).toInt();
+
+    Paint borders = Paint()
       ..color = const Color(0xFFF4EFDC)
       ..style = PaintingStyle.stroke
       ..strokeWidth = 2;
 
-    Path path_0 = Path();
-    path_0.moveTo(size.width - margin, 0);
-    path_0.lineTo(size.width - margin, size.height - margin);
+    Path vLine = Path();
+    vLine.moveTo(hLineLength, 0);
+    vLine.lineTo(hLineLength, vLineLength);
 
-    canvas.drawPath(path_0, paint_0);
+    Path hLine = Path();
+    hLine.moveTo(0, vLineLength);
+    hLine.lineTo(hLineLength, vLineLength);
 
-    // Horizontal line
-    Paint paint_1 = Paint()
-      ..color = const Color(0xFFF4EFDC)
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 2;
+    canvas.drawPath(vLine, borders);
+    canvas.drawPath(hLine, borders);
 
-    Path path_1 = Path();
-    path_1.moveTo(0, size.height - margin);
-    path_1.lineTo(size.width - margin, size.height - margin);
+    for (int i = 1; i <= marksNb; i++) {
+      Path hMark = Path();
+      hMark.moveTo(rulesSpacing * i, vLineLength);
+      hMark.lineTo(rulesSpacing * i, size.height - markLength);
+      canvas.drawPath(hMark, borders);
+    }
 
-    canvas.drawPath(path_1, paint_1);
-
-    Paint paint_2 = Paint()
-      ..color = const Color(0xFFF4EFDC)
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 2;
-
-    Path path_2 = Path();
-    path_2.moveTo(rulesSpacing, size.height - margin);
-    path_2.lineTo(rulesSpacing, size.height - (margin / 2));
-
-    canvas.drawPath(path_2, paint_2);
-
-    Paint paint_3 = Paint()
-      ..color = const Color(0xFFF4EFDC)
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 2;
-
-    Path path_3 = Path();
-    path_3.moveTo(rulesSpacing * 2, size.height - margin);
-    path_3.lineTo(rulesSpacing * 2, size.height - (margin / 2));
-
-    canvas.drawPath(path_3, paint_3);
-
-    Paint paint_4 = Paint()
-      ..color = const Color(0xFFF4EFDC)
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 2;
-
-    Path path_4 = Path();
-    path_4.moveTo(rulesSpacing * 3, size.height - margin);
-    path_4.lineTo(rulesSpacing * 3, size.height - (margin / 2));
-
-    canvas.drawPath(path_4, paint_4);
+    for (int i = 1; i <= vMarksNb; i++) {
+      Path vMark = Path();
+      vMark.moveTo(hLineLength, vLineLength - (rulesSpacing * i));
+      vMark.lineTo(hLineLength + markLength, vLineLength - (rulesSpacing * i));
+      canvas.drawPath(vMark, borders);
+    }
   }
 
   @override
